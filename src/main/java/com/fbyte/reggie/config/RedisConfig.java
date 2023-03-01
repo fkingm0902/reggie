ceb209c085c1daaae5a4a90489fa294f7945ace4
fhjz <hello@fhjz.cn>
2023-03-01 15:43:13 +0800

    Redis的一些配置

---
+++ src/main/java/com/fbyte/reggie/config/RedisConfig.java
@@ -0,0 +1,30 @@
+package com.fbyte.reggie.config;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @author FHJ
+ * @version 1.0
+ * @description Redis配置
+ * @className RedisConfig
+ * @date 2023/3/1 01
+ * @since 1.0
+ */
+
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+
+    @Bean
+    public RedisTemplate<Object,Object> redisTemplate(RedisConnectionFactory connectionFactory){
+        RedisTemplate<Object,Object> redisTemplate = new RedisTemplate<>();
+        // 默认为序列化器为： JdkSerializationRedisSerializer
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+}
